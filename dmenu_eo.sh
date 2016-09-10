88bc351ac8cd7452b172de0fc766ae0681e5b249
Zachary Matthews <zacharymatt5@gmail.com>
2016-09-10 11:00:35 -0400

    Add automatic download of espdic if missing Reorder install script for a more clean method

--- dmenu_eo.sh
+++ dmenu_eo.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
+# ESPDIC download location
+espdic_dl="http://www.denisowski.org/Esperanto/ESPDIC/espdic.txt"
+
 # cache from dmenu_path
 cachedir=${XDG_CACHE_HOME:-"$HOME/.cache"}
 if [ -d "$cachedir" ]; then
 	cache=$cachedir/espdic
 else
 	cache=$HOME/.espdic # if no xdg dir, fall back to dotfile in ~
+fi
+
+# If ESPDIC is installed
+if [ ! -r $cache ]; then
+	wget -O "$cache" $espdic_dl >> /dev/null
+	if [ "$?" -ne 0 ]; then
+		echo "Wget of espdic failed" 1>&2
+		exit 1
+	fi
+	# Convert DOS newline to Unix
+	sed -i 's/.$//' $cache
 fi
 
 cat "$cache" | dmenu -l 10 "$@"
--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-	echo "You must be root to perform this action" 2>&1
+	echo "You must be root to perform this action" 1>&2
 	exit 1
 else
-	# Set permissions
-	chmod 755 "dmenu_eo.sh"
-
-	# Change owner
-	chown root "dmenu_eo.sh"
-
 	# Add to /bin/
 	cp dmenu_eo.sh /bin/dmenu_eo
+
+	# Set permissions
+	chmod 755 "/bin/dmenu_eo"
+
+	# Change owner
+	chown root "/bin/dmenu_eo"
 fi
